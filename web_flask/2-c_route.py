f82dbf8c2577b94e7e27f35907a5291d778222a6
mokhtartouiri <1445@holbertoschool.com>
2021-06-30 15:32:09 +0100

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """
+        hbnb function to return Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index():
+    """
+        /hbnb route
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_is(text):
+    """
+        /C route
+    """
+    return 'C {:s}'.format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
